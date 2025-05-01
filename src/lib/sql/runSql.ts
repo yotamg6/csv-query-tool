918c7ec545fcfd7ad44591d4f0b3689ed1e63c5e
Yotam Gaton <yotamg6@gmail.com>
2025-05-01 20:45:29 +0000

    addd try catch to runSqlQuery

--- src/lib/sql/runSql.ts
+++ src/lib/sql/runSql.ts
@@ -1,13 +1,17 @@
 import { CsvQueryResult, CsvRow } from '@/src/types/csv';
 import alasql from 'alasql/dist/alasql.min.js';
 
-export const runSqlQuery = async (data: CsvRow[], sqlQuery: string): Promise<CsvQueryResult> => {
-  alasql('CREATE TABLE data');
-  alasql.tables.data.data = data;
+export const runSqlQuery = (data: CsvRow[], sqlQuery: string): CsvQueryResult => {
+  try {
+    alasql('CREATE TABLE data;');
+    alasql.tables.data.data = data;
 
-  const result = alasql(sqlQuery) as CsvQueryResult;
-
-  alasql('DROP TABLE data');
-
-  return result;
+    const result = alasql(sqlQuery) as CsvQueryResult;
+    return result;
+  } catch (err: any) {
+    console.error('SQL execution failed:', err.message || err);
+    throw err;
+  } finally {
+    alasql('DROP TABLE data;');
+  }
 };
